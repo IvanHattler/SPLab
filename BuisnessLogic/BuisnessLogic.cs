1053683946a7eca55e81485b95153b9a091964f1
Hattler <ivan.1.99@mail.ru>
2020-03-02 10:57:38 +0400

    start write buisness logic

---
+++ BuisnessLogic/BuisnessLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BuisnessLogic
+{
+    public class DoWhileAnalizer
+    {
+        private bool? isExecuteMoreOne = null;
+        public string Phrase { get; set; }
+        public DoWhileAnalizer(string phrase)
+        {
+            Phrase = phrase;
+        }
+        public bool? IsExecute
+        {
+            get
+            {
+                return isExecuteMoreOne ?? (isExecuteMoreOne = CheckPhrase(Phrase));
+            }
+        }
+
+        private bool? CheckPhrase(string phrase)
+        {
+            return null;
+        }
+    }
+}
