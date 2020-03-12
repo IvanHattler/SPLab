0e18308c6c9a5ffba7803817544b81d118db5857
maks ms <maksim_petryaev@mail.ru>
2020-03-12 21:47:25 +0400

    12341543 7653687u

--- BuisnessLogic/BuisnessLogic.cs
+++ BuisnessLogic/BuisnessLogic.cs
@@ -135,4 +135,25 @@ namespace BuisnessLogic
             return value;
         }
     }
+    public class WhileAnalizer
+    {
+        private string phrase;
+
+        private int count;
+
+        private string Iter;
+
+        public string Phrase { get => phrase; set => phrase = value; }
+        public int Count { get => count; set => count = value; }
+        public string Iter1 { get => Iter; set => Iter = value; }
+
+        string WhileAnalizer(string InputStream)
+        {
+            string i = "while";
+            for(int i=0;i<InputStream.Length;i+=5)
+            {
+                if(i!=InputStream.Substring(i,5))
+            }
+        }
+    }
 }
